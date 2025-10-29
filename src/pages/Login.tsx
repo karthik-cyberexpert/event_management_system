ba478c79e29a2fed413524af1702ebb126b3cfd5
[dyad] <git@dyad.sh>
2025-10-29 11:33:25 +0530

    [dyad] Adding Google Sign-In option - wrote 1 file(s)

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -18,7 +18,7 @@ const Login = () => {
         <Auth
           supabaseClient={supabase}
           appearance={{ theme: ThemeSupa }}
-          providers={[]}
+          providers={['google']}
           theme="light"
         />
       </div>
